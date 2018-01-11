4a5847327161f5008ff664a7c594cbada7585b39
Shasin <shasin89@gmail.com>
2018-01-11 19:14:19 +0800

    Updated build cradle and test

---
+++ horizontalcalendar/src/androidTest/java/devs/mulham/horizontalcalendar/utils/HorizontalCalendarTest.java
@@ -0,0 +1,12 @@
+package devs.mulham.horizontalcalendar.utils;
+
+import android.test.AndroidTestRunner;
+
+/**
+ * Created by Shasin on 11/01/2018.
+ */
+
+
+//@Runwith(AndroidJUnit4.class)
+//public class HorizontalCalendarTest {
+//}
